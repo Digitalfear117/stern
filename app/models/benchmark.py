b5be2bffb62ae5f904d4ff14a12db4eea53f921c
Digitalfear117 <avandenbossche@comcast.net>
2024-09-08 15:03:28 -0400

    Use dictionary for hardware

--- app/models/benchmark.py
+++ app/models/benchmark.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Dict
 
 from .user import UserModel
 
@@ -11,5 +12,5 @@ class BenchmarkModel(BaseModel):
     grade: str
     created_at: datetime
     client: str
-    hardware: str
+    hardware: Dict[str, str]
     user: UserModel
